4a493a71ad0927cdf86f999f0c1e139ee75d3ffc
Kristalina Ratu <94048247+Kristalin12@users.noreply.github.com>
2025-06-27 23:58:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ if uploaded_file is not None:
 
         if y_pred[0] == 1:
             st.markdown("# ⚠️ **Seizure Detected** ⚠️", unsafe_allow_html=True)
-            st.markdown("## **Recommended Action:** Seek immediate medical attention.")
+            st.markdown("### **Recommended Action:** Seek immediate medical attention.")
         else:
             st.markdown("# ✅ **No Seizure Detected**", unsafe_allow_html=True)
-            st.markdown("## **Recommended Action:** Continue monitoring as usual.")
+            st.markdown("### **Recommended Action:** Continue monitoring as usual.")

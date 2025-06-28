de16fe7fd25f3651c6e63684221853dd3ddbf842
Kristalina Ratu <94048247+Kristalin12@users.noreply.github.com>
2025-06-28 20:31:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,68 +4,19 @@ import matplotlib.pyplot as plt
 import joblib 
 import numpy as np
 from tensorflow.keras.models import load_model
+from streamlit_option_menu import option_menu
 
-st.set_page_config(page_title="NeuroScan | Epilepsy AI Dashboard", layout="wide")
+st.set_page_config(page_title="NeuroScan", layout="wide")
 
-st.markdown("""
-    <style>
-    /* Main background */
-    .stApp {
-        background-color: #f9feff !important;
-        color: #080808 !important;
-        font-family: 'Segoe UI', sans-serif;
-    }
-
-    /* Headings */
-    h1, h2, h3 {
-        color: #f9feff;
-    }
-
-    /* Metrics */
-    [data-testid="metric-container"] {
-        background-color: #f9feff;
-        color: #080808;
-        border-radius: 12px;
-        padding: 15px;
-        box-shadow: 0 0 10px rgba(216, 76, 76, 0.3);
-    }
-
-    /* Tabs */
-    .stTabs [role="tablist"] {
-        background: #f9feff;
-        border-radius: 10px;
-    }
-    .stTabs [role="tab"] {
-        color: #f9feff;
-        padding: 10px;
-    }
-    .stTabs [aria-selected="true"] {
-        border-bottom: 4px solid #f9feff;
-        color: #FF9FA2;
-    }
-
-    /* Buttons */
-    .stButton>button {
-        background-color: #D84C4C;
-        color: white;
-        border-radius: 8px;
-        padding: 10px 20px;
-        border: none;
-    }
-    .stButton>button:hover {
-        background-color: #B43E3E;
-        transition: 0.3s ease-in-out;
-    }
-
-    /* Inputs */
-    .css-1cpxqw2 {
-        background-color: #3A1D20 !important;
-        border: 1px solid #EB5456 !important;
-        color: white !important;
-    }
-
-    </style>
-""", unsafe_allow_html=True)
+# sidebar
+with st.sidebar:
+    selected = option_menu(
+        "Menu",
+        ["Dashboard", "Dataset", "Pengujian", "Tentang Peneliti"],
+        icons=["bar-chart", "info-circle", "activity", "check2-square", "envelope"],
+        menu_icon="cast",
+        default_index=0,
+    )
 
 @st.cache_resource
 def load_artifacts():

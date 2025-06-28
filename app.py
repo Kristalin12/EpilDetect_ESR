d910bdbe35e3ffcffdc0d9a18fc801ec7f85b3d7
Kristalina Ratu <94048247+Kristalin12@users.noreply.github.com>
2025-06-28 17:15:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ if uploaded_file is not None:
         y_pred_proba = clf.predict_proba(X_flat)[:, 1]
         y_pred       = (y_pred_proba >= 0.5).astype(int)
         
-        if y_pred[0] == 1:
+        if y_pred_proba[0] == 1:
             st.markdown("# ⚠️ **Seizure Detected** ⚠️", unsafe_allow_html=True)
             st.markdown("#### **Recommended Action:** Seek immediate medical attention.")
         else:

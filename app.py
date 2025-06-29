d5df2b237ddc6a259a16d1de7c1332825fdef25e
Kristalina Ratu <94048247+Kristalin12@users.noreply.github.com>
2025-06-29 20:20:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -274,7 +274,7 @@ elif selected == 'Dataset':
     
     col_bar, col_pie= st.columns([3, 3])        
     with col_pie:
-        fig, ax = plt.subplots(figsize=(3, 3))
+        fig, ax = plt.subplots(figsize=(2.5, 2.5))
         wedges, texts, autotexts = ax.pie(
         counts,
         labels=labels,
@@ -284,8 +284,8 @@ elif selected == 'Dataset':
         textprops={'color': 'black', 'fontsize': 10}
         )
         ax.set_title("Distribusi Kelas", fontsize=10)
-        ax.set_xlabel("Class", fontsize=10)
-        ax.set_ylabel("Count", fontsize=10)
+        ax.set_xlabel(fontsize=10)
+        ax.set_ylabel(fontsize=10)
         ax.axis('equal')
         st.pyplot(fig, use_container_width=True)
     with col_bar:

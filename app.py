1530583cc86d8a64d921ac8ca18fe2344f53433a
Kristalina Ratu <94048247+Kristalin12@users.noreply.github.com>
2025-06-29 15:18:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -194,7 +194,7 @@ if selected == "🏠 Dashboard":
         }
         /* Content styles */
         .content {
-
+            text-align: justify
             background-color: #333;
             box-shadow: 0 4px 12px rgba(0, 0, 0, 0.3);
             margin-bottom: 20px;

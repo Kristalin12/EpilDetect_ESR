1ac6bad14fea31f603ff825dbdd5324ab741dad5
Kristalina Ratu <94048247+Kristalin12@users.noreply.github.com>
2025-06-29 17:51:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -203,22 +203,22 @@ if selected == "Dashboard":
     st.markdown("---")
     st.markdown("""
     <div class='content'>
-        <h2>Penyakit Epilepsi</h2>
+        <h2>Model NeuroScan</h2>
     <div>
     """, unsafe_allow_html=True)
-    with st.expander("Convolutional Autoencoder", expanded=True):
+    with st.expander("## Convolutional Autoencoder", expanded=True):
         st.markdown("""
         Algoritma ini digunakan untuk menurunkan data masukan menjadi fitur representasi yang berdimensi lebih rendah dan memungkinkan algoritma ini untuk mempelajari pola representatif dalam ruang laten.
         """)
-    with st.expander("Logistic Regression", expanded=True):
+    with st.expander("## Logistic Regression", expanded=True):
         st.markdown("""
         Teknik statistik yang dapat menangkap dan menafsirkan hubungan antara prediktor dan tanggapan dikotomis, seperti ada atau tidaknya penyakit.
         """)
-    with st.expander("Support Vector Machine", expanded=True):
+    with st.expander("## Support Vector Machine", expanded=True):
         st.markdown("""
         Algoritma machine learning yang digunakan untuk klasifikasi dan regresi, dengan mencari hyperplane terbaik untuk memisahkan data menjadi beberapa kelas dengan margin yang maksimal.
         """)
-    with st.expander("Soft Voting Classifier", expanded=True):
+    with st.expander("## Soft Voting Classifier", expanded=True):
         st.markdown("""
         Teknik yang melakukan prediksi dengan penggabungan beberapa algoritma secara individu yang kemudian digabungkan untuk mendapatkan jumlah probabilitas yang tertimbang..
         """)

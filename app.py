3f7f2c22ea8b70b6fffd5b8c799b355dbfd109c8
Kristalina Ratu <94048247+Kristalin12@users.noreply.github.com>
2025-06-30 15:44:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -361,7 +361,9 @@ elif selected == 'Tentang Peneliti':
     col1, col2 = st.columns([1, 3])
     with col1:
         st.markdown("""
-            <i class="fa-solid fa-user"></i>
+            <div>
+                <i class="fa-solid fa-user"></i>
+            </div>
         """, unsafe_allow_html=True)
     with col2:
         st.header("Kristalina Chandra Ratu")
